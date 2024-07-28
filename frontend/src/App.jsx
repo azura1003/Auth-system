6db53ebc48df0c02afe8f5f2955110c0577849c4
Azura1003 <noorkhan.95150@gmail.com>
2024-07-28 18:26:03 +0200

    small change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react"
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import Register from "./Register"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <BrowserRouter>
    <Routes>
       <Route path='/' element={<Home />}></Route>
+      <Route path='/register' element={<Register />}></Route>
    </Routes>
    </BrowserRouter>
   )
